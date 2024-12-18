a9139aac249bdaddc4721f5645304243b25da09f
smtpmailers <sandeepsingh@smtpmailers.com>
2024-12-18 15:51:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function startCaptcha() {
 
         // Redirect to the main site after a delay
         setTimeout(() => {
-            window.location.href = "https://refpaiozdg.top/L?tag=d_3758521m_25437c_2840273&site=3758521&ad=25437&r=Registration"; // Redirect URL
+            window.location.href = "https://www.flipkart.com/"; // Redirect URL
         }, 1500); // 1.5 seconds delay before redirect
     }, 2000); // Simulated processing time (2 seconds)
 }
